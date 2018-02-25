039c998d7882df652c22ceea47d34a3d32546d9e
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2018-02-25 22:44:07 +0100

    Initial commit of projects local files

---
+++ js/scroll.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    selected = 0;
+    moving = false;
+
+    $('nav a').click(function (event) {
+        event.preventDefault();
+        moving = false;
+        selected = $(this).attr('href').substr(2);
+        panelheight = $('#a' + selected).height();
+        winheight = $(window).height();
+        offset = (winheight - panelheight) / 2;
+        if (offset < 0) {
+            offset = 0;
+        }
+        distance = $('#a' + selected).offset().top - offset;
+        $('html, body').animate({
+            scrollTop: distance
+        }, 750);
+    });
+
+    $(window).scroll(function () {
+        moving = true;
+        toppos = $('html, body').scrollTop();
+    });
+
+    $(window).resize(function () {
+        if (selected > 0 && !moving) {
+            panelheight = $('#a' + selected).height();
+            winheight = $(window).height();
+            offset = (winheight - panelheight) / 2;
+            if (offset < 0) {
+                offset = 0;
+            }
+            distance = $('#a' + selected).offset().top - offset;
+            $('html, body').scrollTop(distance);
+        }
+    }).resize();
+
+
+});
